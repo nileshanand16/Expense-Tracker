768d9e38d4cc56c22a08f2be1a702e9aa2624a3d
Nilesh Anand <nileshanand16@gmail.com>
2022-01-21 22:48:53 +0530

    update

---
+++ src/Components/IncomeExpense.js
@@ -0,0 +1,20 @@
+import React, { useContext } from 'react'
+import { GlobalContext } from '../context/GlobalState';
+
+
+export const IncomeExpense = () => {
+  const { transactions } = useContext(GlobalContext);
+
+    return (
+        <div className="inc-exp-container">
+        <div>
+          <h4>Income</h4>
+          <p id="money-plus" className="money plus">+$0.00</p>
+        </div>
+        <div>
+          <h4>Expense</h4>
+          <p id="money-minus" className="money minus">-$0.00</p>
+        </div>
+      </div>
+    )
+}
