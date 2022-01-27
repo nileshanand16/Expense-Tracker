3a78996d7999dbb49b095af763e7f8d747ec0811
Nilesh Anand <nileshanand16@gmail.com>
2022-01-27 22:37:22 +0530

    MERN update

--- client/src/Components/IncomeExpense.js
+++ client/src/Components/IncomeExpense.js
@@ -1,20 +1,6 @@
 import React, { useContext } from "react";
 import { GlobalContext } from "../context/GlobalState";
-
-function moneyFormatter(num) {
-  let p = num.toFixed(2).split(".");
-  return (
-    "₹ " +
-    p[0]
-      .split("")
-      .reverse()
-      .reduce(function (acc, num, i, orig) {
-        return num === "-" ? acc : num + (i && !(i % 3) ? "," : "") + acc;
-      }, "") +
-    "." +
-    p[1]
-  );
-}
+import { numberWithCommas } from "../utils/format";
 
 export const IncomeExpense = () => {
   const { transactions } = useContext(GlobalContext);
@@ -23,21 +9,23 @@ export const IncomeExpense = () => {
 
   const income = amounts
     .filter((item) => item > 0)
-    .reduce((acc, item) => (acc += item), 0);
+    .reduce((acc, item) => (acc += item), 0)
+    .toFixed(2);
 
-  const expense =
+  const expense = (
     amounts.filter((item) => item < 0).reduce((acc, item) => (acc += item), 0) *
-    -1;
+    -1
+  ).toFixed(2);
 
   return (
     <div className="inc-exp-container">
       <div>
         <h4>Income</h4>
-        <p className="money plus">{moneyFormatter(income)}</p>
+        <p className="money plus">₹{numberWithCommas(income)}</p>
       </div>
       <div>
         <h4>Expense</h4>
-        <p className="money minus">{moneyFormatter(expense)}</p>
+        <p className="money minus">₹{numberWithCommas(expense)}</p>
       </div>
     </div>
   );
--- client/src/Components/Transaction.js
+++ client/src/Components/Transaction.js
@@ -1,20 +1,6 @@
 import React, { useContext } from "react";
 import { GlobalContext } from "../context/GlobalState";
-
-function moneyFormatter(num) {
-  let p = num.toFixed(2).split(".");
-  return (
-    "₹ " +
-    p[0]
-      .split("")
-      .reverse()
-      .reduce(function (acc, num, i, orig) {
-        return num === "-" ? acc : num + (i && !(i % 3) ? "," : "") + acc;
-      }, "") +
-    "." +
-    p[1]
-  );
-}
+import { numberWithCommas } from "../utils/format";
 
 export const Transaction = ({ transaction }) => {
   const { deleteTransaction } = useContext(GlobalContext);
@@ -25,11 +11,10 @@ export const Transaction = ({ transaction }) => {
     <li className={transaction.amount < 0 ? "minus" : "plus"}>
       {transaction.text}{" "}
       <span>
-        {sign}
-        {moneyFormatter(transaction.amount)}
+        {sign}₹{numberWithCommas(Math.abs(transaction.amount))}
       </span>
       <button
-        onClick={() => deleteTransaction(transaction.id)}
+        onClick={() => deleteTransaction(transaction._id)}
         className="delete-btn"
       >
         x
